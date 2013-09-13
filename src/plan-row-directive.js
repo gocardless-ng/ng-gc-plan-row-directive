489899ff331bbb121e9d7f131004ef62b132a9f1
Philip Harrison <philip@mailharrison.com>
2013-09-13 19:14:41 +0100

    Initial

---
+++ src/plan-row-directive.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('gc.planRow', [
+  'gc.intervals',
+  'ngGcCapitalize',
+  'ngGcCapitalizeEach',
+  'plan-row-template.html'
+])
+.directive('planRow', [
+  function planRowDirective() {
+
+    return {
+      restrict: 'E',
+      templateUrl: 'plan-row-template.html',
+      scope: {
+        plan: '=',
+        isAuthorization: '@',
+        url: '@'
+      },
+      replace: true
+    };
+
+  }
+]);
